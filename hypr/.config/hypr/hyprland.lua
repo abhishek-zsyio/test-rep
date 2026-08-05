958d08216bbe8d71f45165f06847287475498f0b
Abhishek Zsyio <abhishek@zsyio.com>
2026-08-05 15:44:22 +0530

    Add hyprland.lua entry point for Hyprland 0.55+ compatibility

---
+++ hypr/.config/hypr/hyprland.lua
@@ -0,0 +1,11 @@
+-- Hyprland Lua Configuration Entry Point (Hyprland 0.55+)
+
+-- Sourcing modular configurations
+hl.source("conf/colors.conf")
+hl.source("conf/env.conf")
+hl.source("conf/monitors.conf")
+hl.source("conf/options.conf")
+hl.source("conf/animations.conf")
+hl.source("conf/windowrules.conf")
+hl.source("conf/keybindings.conf")
+hl.source("conf/startup.conf")
